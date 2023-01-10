b43198d6a77f9b54320edfe284f562d2b0137580
unknown <matt.bedford@gmail.com>
2023-01-10 23:13:15 +0100

    Restructured static assets, added JS, built more sections

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+function animateValue2(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+function animateValue3(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+function animateValue4(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+var yesbar = false; 
+window.onscroll = function() {scrollHeader()};
+
+function scrollHeader() {
+if (yesbar == false) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+const obj = document.getElementById("value");
+animateValue(obj, 0, 7, 2500);
+const obj4 = document.getElementById("value4");
+animateValue4(obj4, 0, 70, 3000);
+const obj2 = document.getElementById("value2");
+animateValue2(obj2, 0, 160, 3000);
+const obj3 = document.getElementById("value3");
+animateValue3(obj3, 0, 11, 2000);
+yesbar = true;
+}
+}
+}
